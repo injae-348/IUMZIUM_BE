0c2d58b852a70dbf35be09c4c03ae76c20c0f331
injae-348 <ant0593@naver.com>
2024-08-13 23:28:15 +0900

    feat: CORS 정책 추가, openai, flask_cors import

--- test_openapi/app.py
+++ test_openapi/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from routes import api_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
+# 특정 도메인만 허용
+# CORS(app, resources={r"/api/*": {"origins": "https://example.com"}})
 
 app.register_blueprint(api_bp)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
