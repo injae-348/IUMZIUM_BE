9d9bd84db3d70ee4d5f50e9470eacf84e4bf6af6
injae-348 <ant0593@naver.com>
2024-08-20 00:49:34 +0900

    fix: OPTIONS 설정을 통한 Preflight 에러 해결

--- test_openapi/routes/audioRoutes.py
+++ test_openapi/routes/audioRoutes.py
@@ -3,8 +3,16 @@ from services.audioService import process_audio_file
 
 audio_bp = Blueprint('audio', __name__)
 
-@audio_bp.route("/", methods=["POST"])
+@audio_bp.route("/", methods=["POST", "OPTIONS"])
 def audiobot():
+    if request.method == "OPTIONS":
+        # OPTIONS 요청에 대한 응답
+        response = jsonify({})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        return response
+    
     if 'file' not in request.files:
         return jsonify({"error": "파일이 포함되지 않았습니다."}), 400
 
--- test_openapi/routes/chatRoutes.py
+++ test_openapi/routes/chatRoutes.py
@@ -3,10 +3,16 @@ from services.chatService import handle_chat
 
 chat_bp = Blueprint('chat', __name__)
 
-@chat_bp.route("/", methods=["POST"])
+@chat_bp.route("/", methods=["POST", "OPTIONS"])
 def chatbot():
-    user_input = request.json.get("message")
+    if request.method == 'OPTIONS':
+        response = jsonify({})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        return response
 
+    user_input = request.json.get("message")
     if not user_input:
         return jsonify({"error": "'message' 필드가 필요합니다."}), 400
 
